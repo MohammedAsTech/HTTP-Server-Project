ade07fc6088f9d063c9def7e85fa1b65b9dc8af5
MohammedAsTech <mmm579aiman@gmail.com>
2026-06-25 17:56:59 +0300

    added router h and cpp

--- include/Router.h
+++ include/Router.h
@@ -1,8 +1,18 @@
-//
-// Created by moham on 25/06/2026.
-//
-
 #ifndef ROUTER_H
 #define ROUTER_H
+#include "Handler.h"
+#include <map>
+#include <string>
+#include <memory>
 
-#endif //ROUTER_H
+class Router {
+public:
+    Router();
+    void addRoute(const std::string& method, const std::string& path, std::shared_ptr<Handler> handler);
+    Handler* dispatch(const HttpRequest& req);
+
+private:
+    std::map<std::pair<std::string, std::string>, std::shared_ptr<Handler>> m_routes;
+    std::shared_ptr<Handler> m_notFoundHandler;
+};
+#endif
--- src/Router.cpp
+++ src/Router.cpp
@@ -1,3 +1,15 @@
-//
-// Created by moham on 25/06/2026.
-//
+#include "Router.h"
+
+Router::Router() : m_notFoundHandler(std::make_shared<NotFoundHandler>()) {}
+
+void Router::addRoute(const std::string& method, const std::string& path, std::shared_ptr<Handler> handler) {
+    m_routes[{method, path}] = handler;
+}
+
+Handler* Router::dispatch(const HttpRequest& req) {
+    auto it = m_routes.find({req.method, req.path});
+    if (it != m_routes.end()) {
+        return it->second.get();
+    }
+    return m_notFoundHandler.get();
+}
