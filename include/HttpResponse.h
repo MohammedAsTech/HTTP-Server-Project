9596b70388b40dd16d62ecf2209f7d22f4294d43
MohammedAsTech <mmm579aiman@gmail.com>
2026-07-06 17:19:21 +0300

    finishing a proper httpResponse.h

--- include/HttpResponse.h
+++ include/HttpResponse.h
@@ -1,10 +1,25 @@
 #ifndef HTTP_RESPONSE_H
 #define HTTP_RESPONSE_H
 #include <string>
+#include <map>
 
-struct HttpResponse {
-    int statusCode = 200;
-    std::string statusText = "OK";
-    std::string body;
+class HttpResponse {
+public:
+    HttpResponse();
+    void setStatus(int code, const std::string& text);
+    void setHeader(const std::string& key, const std::string& value);
+    void setBody(const std::string& body);
+    std::string build();
+
+    HttpResponse& ok(const std::string& body = "");
+    HttpResponse& notFound();
+    HttpResponse& badRequest();
+    HttpResponse& serverError();
+
+private:
+    int m_statusCode;
+    std::string m_statusText;
+    std::map<std::string, std::string> m_headers;
+    std::string m_body;
 };
 #endif
